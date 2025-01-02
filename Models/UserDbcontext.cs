538b2ad320b240abc834c1729c9242b2dc32d651
Esraa <esraasherif1042@gmail.com>
2025-01-02 10:25:14 +0200

    Migration

---
+++ Models/UserDbcontext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace user_registration.Models
+{
+    public class UserDbcontext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;Database=user_registration;Trusted_Connection=true;TrustServerCertificate=true;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
