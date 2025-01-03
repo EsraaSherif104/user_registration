2fef03358703dfef9b9089d9fe6081e024a5a9b6
esraa sherif <119526601+EsraaSherif104@users.noreply.github.com>
2025-01-03 12:49:55 +0200

    Update Problem Solving.cs

--- Problem Solving.cs
+++ Problem Solving.cs
@@ -5,12 +5,13 @@ public class Test
 public static int operation (List<string> list){
 
 int count=0;
+	if(list is not null){
 for(int i=0;i<list.Count;i++){
    if(list[i]=="++")
    count+=1;
    else if(list[i]=="--")
    count-=1;
-   }
+   }}
    return count;
 
 }
